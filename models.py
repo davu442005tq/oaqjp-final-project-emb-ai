30448b0a2ce584900b60282b5046d6e2b26b4c50
duonganhvu <duonganhvu555@gmail.com>
2026-06-28 15:06:41 +0700

    Add Course model, update Lesson, add course_details_bootstrap.html template

--- models.py
+++ models.py
@@ -32,7 +32,17 @@ class Submission(models.Model):
         return f"{self.user.username} - {self.question.question_text[:50]}"
 
 
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons')
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
